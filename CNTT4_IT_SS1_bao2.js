90ca19af050b4883de608c2ab00be196b03f9ec5
empedangiunhatptit <phamngoclinhtq2006@gmail.com>
2025-08-04 12:45:11 +0700

    Update CNTT4_IT_SS1_bao2.js

--- CNTT4_IT_SS1_bao2.js
+++ CNTT4_IT_SS1_bao2.js
@@ -1,8 +1,10 @@
 const arr=[1,2,3];
-console.log("gia tri ban dau");
-console.log("a=",a);
+let a= 10;  //khai bao bien a
+console.log("gia tri ban dau"); //in ra gia tri ban dau
+console.log("a=",a); // in ra gia tri cua bien a
+//in mang
 console.log(arr);
-arr.push(4);
-console.log("gia tri sau khi push");
-console.log("a=",a);
+arr.push(4);  //them phan tu vao cuoi mang
+console.log("gia tri sau khi push");  //in ra gia tri sau khi push
+console.log("a=",a);  //in ra gia tri cua bien a
 console.log(arr);
