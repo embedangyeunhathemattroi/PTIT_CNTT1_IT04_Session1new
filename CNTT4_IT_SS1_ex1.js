f90a593e9e988a86dc221faf6e09403c2f082c59
empedangiunhatptit <phamngoclinhtq2006@gmail.com>
2025-08-04 12:44:09 +0700

    Update CNTT4_IT_SS1_ex1.js

--- CNTT4_IT_SS1_ex1.js
+++ CNTT4_IT_SS1_ex1.js
@@ -1,4 +1,8 @@
+//vong lap for vs bien i khai bao let
 for(let i=0;i<=5;i++){
+    //vong lap in ra gtri hien tai cua i
     console.log(i);
 }
+//vi bien i dc khai bao bang let chi co pvi trong block, nen ra ngoai vong fo bien i ko ton tai
+
 console.log("ngoai vong lap i =",typeof i);
