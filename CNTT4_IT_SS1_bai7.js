8c402e3dc4899a6b51a14872396eb17c8c9b6c99
empedangiunhatptit <phamngoclinhtq2006@gmail.com>
2025-08-04 12:43:27 +0700

    Update CNTT4_IT_SS1_bai7.js

--- CNTT4_IT_SS1_bai7.js
+++ CNTT4_IT_SS1_bai7.js
@@ -1,16 +1,4 @@
-function sumArr(){
-    const result=[];
-    for(let i=0;i<arguments.length;i++){
-        const arr=arguments[i];
-        if(Array.isArray(arr)){
-            const sum=arr.reduce((a,b)=>a+b,0);
-            result.push(sum);
-        }else{
-            console.log(`Tham so thu ${i+1} khong phai mang`);
-        }
-    }
-    return result;
+const sumArrays = (...Arrays)=>{
+    return Arrays.map(arr=>arr.reduce((sum,val)=> sum+val,0));
 }
-
-const result= sumArrays([1,2],[6,7,8],[12,9]);
-console.log(result);
+console.log(sumArrays([1,2,3],[2,3]));
